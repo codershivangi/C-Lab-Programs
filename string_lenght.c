0a72a9354252ace88d81c8ef5d62a258618bed6a
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-03-21 23:45:55 +0530

    Add files via upload

---
+++ string_lenght.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+ int main(){
+
+     char arr[]="Hello World";
+     int lenght;
+     lenght=strlen(arr);
+     printf("lenght=%d",lenght);
+     return 0;
+     
+}
