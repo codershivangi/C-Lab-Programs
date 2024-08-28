01045bf6c4e95d4c472747fc5d9edb3466e78410
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-08-28 09:53:02 -0700

    Add files via upload

---
+++ Implicit_conversion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main()
+ {
+
+    int x = 10;          // integer x
+    char y = 'a';        // character c
+    // y implicity converted to int.ASCII
+    // value of 'a' is 97
+    x = x + y;
+
+    // x is implicity converted to float
+    float z = x + 1.0;
+
+    printf("x=%d,z=%f",x,z);
+    return 0;
+
+ }
